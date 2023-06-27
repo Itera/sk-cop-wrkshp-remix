1d159377b18dfeaf3dc8d1f9616fc5388d2bbba4
Ludovit Mydla <Ludovit.Mydla@gjensidige.no>
2023-06-27 16:58:18 +0200

    feat: added styling

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,11 @@
-import {
-  Outlet,
-} from "@remix-run/react";
+import { Links, Outlet } from "@remix-run/react";
+
 
 export default function App() {
   return (
     <html lang="en">
       <head>
-
+        <Links />
       </head>
       <body>
         {'There will be something below ⬇'}
--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,6 +1,11 @@
 import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
 import { getPlayerInfo } from "~/models/player.server";
+import styles from "~/styles/styles.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
 
 export const loader = async () => {
   return json({ player: await getPlayerInfo() });
@@ -14,6 +19,13 @@ export default function Settings() {
       <h1>Settings</h1>
       <p>TODO: set player name here (current: {player.name})</p>
       <Link to="/">Go back</Link>
+      <br />
+      <div className={'paddedBox'}>
+        <label htmlFor={'player.name'}>Player name is:</label>
+        <input name={'player.name'} type='text'/>
+        <br />
+        <button>Save</button>
+      </div>
     </div>
   );
 }
